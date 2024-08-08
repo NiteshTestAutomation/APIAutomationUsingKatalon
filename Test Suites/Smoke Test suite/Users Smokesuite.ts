39987ec761cad50bad0c65b5fcb8d301f25df7a4
NiteshTestAutomation <niteshtestautomation@gmail.com>
2024-08-08 15:03:22 +0300

    List All users Get Request Test case

---
+++ Test Suites/Smoke Test suite/Users Smokesuite.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Users Smokesuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>df44be97-99a2-4219-b596-7c269694cc3a</testSuiteGuid>
+   <testCaseLink>
+      <guid>9c051c34-d949-450d-92ca-0edd8c26500e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ReqRes Users TestCases/List Users Testcase</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
